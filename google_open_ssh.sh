69326ce8f8a2b3721c876187389503b152c52d51
ziningdada <pznforwork@outlook.com>
2020-03-03 15:59:04 +0800

    Update google_open_ssh.sh

--- google_open_ssh.sh
+++ google_open_ssh.sh
@@ -1,4 +1,6 @@
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 service sshd restart
+echo "123456" | passwd --stdin root #centos
+echo root:123456 | chpasswd #ubuntu
 echo "ok"
