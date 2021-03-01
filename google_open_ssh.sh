7954ba9a8018f5739801b131e5a76aae4b8aabc6
ziningdada <pznforwork@outlook.com>
2021-03-01 23:10:49 +0800

    Update google_open_ssh.sh

--- google_open_ssh.sh
+++ google_open_ssh.sh
@@ -1,6 +1,7 @@
 sed -i '1i\PermitRootLogin yes' /etc/ssh/sshd_config
 sed -i '2i\PasswordAuthentication yes' /etc/ssh/sshd_config
 service sshd restart
+#apt-get install wget
 echo "Google$3380058" | passwd --stdin root #centos
 echo root:"Google$3380058" | chpasswd #ubuntu,现在应该不行得输入passwd改
 echo "ok"
